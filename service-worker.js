9661d1fc3c45a21889e422fd65504f84514a42de
Shawn Littrel <shawnlittrel@protonmail.com>
2021-05-31 17:47:57 -0700

    add offline functionality

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,67 @@
-const APP_PREFIX = 'FoodFest-';
-const VERSION = 'version_01';
+const APP_PREFIX = "FoodFest-";
+const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-     './index.html',
-     './events.html',
-     './tickets.html',
-     './schedule.html',
-     './assets/css/style.css',
-     './assets/css/bootstrap.css',
-     './assets/css/tickets.css',
-     './dist/app.bundle.js',
-     './dist/events.bundle.js',
-     './dist/tickets.bundle.js',
-     './dist/schedule.bundle.js'
+  "./index.html",
+  "./events.html",
+  "./tickets.html",
+  "./schedule.html",
+  "./assets/css/style.css",
+  "./assets/css/bootstrap.css",
+  "./assets/css/tickets.css",
+  "./dist/app.bundle.js",
+  "./dist/events.bundle.js",
+  "./dist/tickets.bundle.js",
+  "./dist/schedule.bundle.js",
 ];
 
-self.addEventListener('install', function(e) {
-     e.waitUntil(
-          caches.open(CACHE_NAME).then(function (cache) {
-               console.log('installing cache : ' + CACHE_NAME)
-               return cache.addAll(FILES_TO_CACHE)
-          })
-     )
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      let cacheKeeplist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+
+        //does not return until all promises are resolved or rejected.
+        //if key cannot be found in cache keeplist, delete key from cache
+      return Promise.all(
+        keyList.map(function(key, i) {
+          //returns -1 if key is not found in keeplist
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", function(e) {
+  console.log("fetch request: " + e.request.url);
+  e.respondWith(
+    //match requested resource with resources in cache
+    caches.match(e.request).then(function(request) {
+      //if cache is available, respond with cache
+      if (request) {
+        console.log("responding with cache: " + e.request.url);
+        return request;
+        //otherwise perform fetch
+      } else {
+        console.log("file is not cached, fetching : " + e.request.url);
+        return fetch(e.request);
+      }
+    })
+  );
 });
