1705074d86c11470b641018e237ef0e7d257741c
Shawn Littrel <shawnlittrel@protonmail.com>
2021-05-31 20:12:25 -0700

    page not loading

--- webpack.config.js
+++ webpack.config.js
@@ -49,18 +49,18 @@ module.exports = {
   }),
 
   new WebpackPwaManifest({
-    name: 'Food Event',
-    short_name: 'Foodies', 
-    description: 'An app that allows you to view upcoming food events.',
-    start_url: '../index.html',
-    background_color: '#01579b',
-    theme_color: '#ffffff',
+    name: "Food Event",
+    short_name: "Foodies",
+    description: "An app that allows you to view upcoming food events.",
+    start_url: "../index.html",
+    background_color: "#01579b",
+    theme_color: "#ffffff",
     fingerprints: false,
     inject: false,
     icons: [{
-      src: path.resolve('assets/img/icons/icon-512x512.png'),
+      src: path.resolve("assets/img/icons/icon-512x512.png"),
       sizes: [96, 128, 192, 256, 384, 512],
-      destination: path.join('assets', 'icons')
+      destination: path.join("assets", "icons")
     }]
   })
   ],
